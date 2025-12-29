ecb3687235deb560d4b7eac7a5e1945f2d740cc3
Limax <limax.workspace@gmail.com>
2025-12-29 19:44:49 +0100

    chore: change api to use env var instead of hardcoded var

--- frontend/services/apiService.ts
+++ frontend/services/apiService.ts
@@ -1,6 +1,7 @@
 import { FamilyMember, Plant, Chore } from '../lib/types';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+console.log(API_BASE_URL)
 
 interface DashboardResponse {
   duePlants: [number, string, number, number, string | undefined, number][];
