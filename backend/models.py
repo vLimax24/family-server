fe2ec70c4acb599ccc8dc8ced2c92d84ee95168b
Limax <limax.workspace@gmail.com>
2026-01-25 19:59:56 +0100

    feat: add models for for items and recipes

--- backend/models.py
+++ backend/models.py
@@ -75,5 +75,62 @@ sql_statements = [
         was_overdue INTEGER DEFAULT 0,
         points INTEGER DEFAULT 1,
         FOREIGN KEY(completed_by) REFERENCES person(id)
+    );""",
+    # Einkaufsliste
+
+    """CREATE TABLE IF NOT EXISTS shopping_master_items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        category TEXT NOT NULL,
+        default_quantity REAL DEFAULT 1,
+        default_unit TEXT DEFAULT 'x',
+        sort_order INTEGER NOT NULL,
+        created_at INTEGER NOT NULL
+    );""",
+    """CREATE TABLE IF NOT EXISTS shopping_sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        type TEXT NOT NULL,
+        status TEXT NOT NULL DEFAULT 'active',
+        created_by INTEGER NOT NULL,
+        created_at INTEGER NOT NULL,
+        completed_at INTEGER,
+        FOREIGN KEY(completed_by) REFERENCES person(id)
+    );""",
+    """CREATE TABLE IF NOT EXISTS shopping_session_items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        session_id INTEGER NOT NULL,
+        master_item_id INTEGER, -- NULL for spontaneous items due to not being inside master items table
+        name TEXT NOT NULL,
+        quantity REAL NOT NULL,
+        unit TEXT NOT NULL,
+        category TEXT NOT NULL,
+        is_checked BOOLEAN DEFAULT 1,
+        notes TEXT,
+        added_by INTEGER NOT NULL,
+        recipe_id INTEGER, -- NULL if item is not from a recipe
+        created_at INTEGER NOT NULL,
+        updated_at INTEGER NOT NULL,
+        FOREIGN KEY(session_id) REFERENCES shopping_sessions(id),
+        FOREIGN KEY(master_item_id) REFERENCES master_items(id),
+        FOREIGN KEY(added_by) REFERENCES person(id),
+        FOREIGN KEY(recipe_id) REFERENCES recipes(id)
+    );""",
+    """CREATE TABLE IF NOT EXISTS recipes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+        servings INTEGER DEFAULT 4,
+        created_at INTEGER NOT NULL,
+        created_by INTEGER NOT NULL,
+        FOREIGN KEY(created_by) REFERENCES person(id)
+    );""",
+    """CREATE TABLE IF NOT EXISTS recipe_ingredients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        recipe_id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        quantity REAL NOT NULL,
+        unit TEXT NOT NULL,
+        category TEXT, -- for sorting
+        FOREIGN KEY(recipe_id) REFERENCES recipes(id)
     );"""
 ]
